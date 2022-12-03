8adc142765798ead55573865655bd88e809a877b
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-12-03 14:17:38 +0100

    Day 3 part 2

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -1,31 +1,26 @@
 fn main() {
-    let input: Vec<(&str, &str)> = include_str!("./day_03.input")
+    let input: Vec<&str> = include_str!("./day_03.input")
         .trim()
         .split("\n")
-        .map(|x| x.split_at(x.len()/2))
         .collect();
 
     let mut prios: Vec<u32> = vec![];
 
-    for i in input {
+    for i in (0..input.len()).step_by(3){
         let mut duplicate: u8 = 0;
-        for j in i.0.chars() {
-            for k in i.1.chars() {
-                if j == k {
-                    if j as u8 > 'a' as u8 {
-                        duplicate = j as u8 - 96;
-                        break;
-                    }
-                    duplicate = j as u8 - (64 - 26);
+        for j in input[i].chars() {
+            if input[i+1].contains(j) && input[i+2].contains(j) {
+                if j as u8 > 'a' as u8 {
+                    duplicate = j as u8 - 96;
+                    break;
                 }
-            }
-            if duplicate != 0 {
-                prios.push(duplicate as u32);
+                duplicate = j as u8 - (64 - 26);
                 break;
             }
         }
+        prios.push(duplicate as u32);
     }
-    println!("{:?}", prios);
+
     let sum: u32 = prios.iter().sum();
 
     println!("{}", sum);
