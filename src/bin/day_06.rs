00d7ed244e0cdd29f00141fce59fd2dce9cbc456
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-12-06 09:29:06 +0100

    Day 6 part 2

--- src/bin/day_06.rs
+++ src/bin/day_06.rs
@@ -1,12 +1,12 @@
 fn main() {
     let input: Vec<char> = include_str!("./day_06.input").trim().chars().collect();
 
-    let mut diff_to_go = 3;
-    for i in 0..input.len()-3 {
+    let mut diff_to_go = 13;
+    for i in 0..input.len()-13 {
         if !input[i+1..=i+diff_to_go].contains(&input[i]) {
             if diff_to_go == 0 {
                 println!("{}", i+1);
-                // for j in i-3..=i {
+                // for j in i-13..=i {
                 //     print!("{:?}", input[j]);
                 // }
                 break;
@@ -14,6 +14,6 @@ fn main() {
             diff_to_go -= 1;
             continue;
         }
-        diff_to_go = 3;
+        diff_to_go = 13;
     }
 }
