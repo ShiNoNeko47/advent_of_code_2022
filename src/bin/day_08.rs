58d41c0338b16d29d6362b09ff05d3c4e0b2cae3
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-12-08 10:44:41 +0100

    Day 8 part 2

--- src/bin/day_08.rs
+++ src/bin/day_08.rs
@@ -9,23 +9,39 @@ fn main() {
     let x: usize = input[0].len();
     let y: usize = input.len();
 
-    let mut result: usize = x * y;
+    let mut result: usize = 0;
 
     for i in 1..y-1 {
         for j in 1..x-1 {
-            let height: u8 = input[i][j];
+            let tree_height: u8 = input[i][j];
             let vertical: &Vec<u8> = &input.iter().map(|x| x[j]).collect();
             let horizontal: &Vec<u8> = &input[i];
 
-            let max_heights: [&u8; 4] = [
-                    vertical[0..i].iter().max().unwrap(),
-                    vertical[i+1..].iter().max().unwrap(),
-                    horizontal[0..j].iter().max().unwrap(),
-                    horizontal[j+1..].iter().max().unwrap(),
+            let mut heights: Vec<Vec<u8>> = vec![
+                    vertical[0..i].to_vec(),
+                    vertical[i+1..].to_vec(),
+                    horizontal[0..j].to_vec(),
+                    horizontal[j+1..].to_vec(),
                 ];
 
-            if max_heights.iter().min().unwrap().to_owned() >= &height {
-                result -= 1;
+            heights[0].reverse();
+            heights[2].reverse();
+
+            let mut scenic_directions: [usize; 4] = [0, 0, 0, 0];
+
+            for k in 0..4 {
+                for height in heights[k].iter() {
+                    scenic_directions[k] += 1;
+                    if height >= &tree_height {
+                        break;
+                    }
+                }
+            }
+
+            let scenic_score: usize = scenic_directions.iter().product();
+
+            if scenic_score > result {
+                result = scenic_score;
             }
 
         }
